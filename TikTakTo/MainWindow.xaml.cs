76929237b9ca5078cf6c271234f0252272e6c3ce
K.Bence <kissbence225@gmail.com>
2024-04-25 11:59:08 +0200

    alapok

--- TikTakTo/MainWindow.xaml.cs
+++ TikTakTo/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace TikTakTo
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int size = 3;
+        int[,] matrix;
+
         public MainWindow()
         {
             InitializeComponent();
+            for (int i = 0; i < size; i++)
+            {
+                JatekGrid.RowDefinitions.Add(new());
+                JatekGrid.ColumnDefinitions.Add(new());
+            }
+            GenerateMatrix();
+            ShowMatrix();
+        }
+
+        public void GenerateMatrix()
+        {
+            matrix = new int[size, size];
+            Random r = new();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = r.Next(1, 9);
+                }
+            }
+        }
+
+        public void ShowMatrix()
+        {
+            JatekGrid.Children.Clear();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Button b = new();
+                    b.Content = matrix[i, j];
+                    b.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    b.VerticalAlignment = VerticalAlignment.Stretch;
+                    Grid.SetRow(b, i);
+                    Grid.SetColumn(b, j);
+                    b.Click += lerak_Click;
+                    JatekGrid.Children.Add(b);
+                }
+            }
+        }
+
+        private void lerak_Click(object sender, RoutedEventArgs e)
+        {
+            Button current = sender as Button;
+            int row = Grid.GetRow(current);
+            int column = Grid.GetColumn(current);
+            ShowMatrix();
         }
     }
 }
