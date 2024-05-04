bc2c361fe83b00785aec10219769465012a55b0f
K.Bence <kissbence225@gmail.com>
2024-05-04 15:13:04 +0200

    kukaba minden ---> amőba

--- TikTakTo/MainWindow.xaml.cs
+++ TikTakTo/MainWindow.xaml.cs
@@ -20,70 +20,14 @@ namespace TikTakTo
     /// </summary>
     public partial class MainWindow : Window
     {
-        const int size = 3;
-        int[,] matrix;
-
         public MainWindow()
         {
             InitializeComponent();
-            for (int i = 0; i < size; i++)
-            {
-                JatekGrid.RowDefinitions.Add(new());
-                JatekGrid.ColumnDefinitions.Add(new());
             }
-            GenerateMatrix();
-            ShowMatrix();
-        }
 
-        public void GenerateMatrix()
+        private void mezo11_Click(object sender, RoutedEventArgs e)
         {
-            int[] szamok = { 1, 2, 3, 4, 5, 6, 7, 8,9 };
-            int[] shuffled = szamok.OrderBy(n => Guid.NewGuid()).ToArray();
-            int counter = 0;
-            matrix = new int[size, size];
-            Random r = new();
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    matrix[i, j] = shuffled[counter];
-                    counter++;
-                }
-            }
-        }
 
-        public void ShowMatrix()
-        {
-            JatekGrid.Children.Clear();
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    Button b = new();
-                    b.Content = matrix[i, j];
-                    if (matrix[i, j] == 9)
-                    {
-                        b.Background = new SolidColorBrush(Colors.Gray); b.Foreground= new SolidColorBrush(Colors.Gray);
-                        b.IsEnabled = false;
-                    }
-                    b.HorizontalAlignment = HorizontalAlignment.Stretch;
-                    b.VerticalAlignment = VerticalAlignment.Stretch;
-                    Grid.SetRow(b, i);
-                    Grid.SetColumn(b, j);
-                    b.Click += csere_Click;
-                    JatekGrid.Children.Add(b);
-                }
-            }
-        }
-
-        private void csere_Click(object sender, RoutedEventArgs e)
-        {
-            Button current = sender as Button;
-            int row = Grid.GetRow(current);
-            int column = Grid.GetColumn(current);
-            if (current.IsEnabled == true &&)
-
-            ShowMatrix();
         }
     }
 }
